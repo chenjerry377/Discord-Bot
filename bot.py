9a7f35f4f4f053cef359ff94354264cf98e0412b
chenjerry377 <112854866+chenjerry377@users.noreply.github.com>
2023-08-26 16:48:58 -0400

    Update bot.py
    added free api to generate quotes and added a set of quotes that are used to respond to certain keywords

--- bot.py
+++ bot.py
@@ -1,9 +1,26 @@
 import discord
+import json
+import requests
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents = intents)
+
+key_words = ["inspire", "sad", "pressured", "despressed", "encouragement", "encourage", "inspirebot", "inspirationalbot"]
+
+start_quotes = [ 
+    "You can do it",
+    "Keep fighting",
+    "Believe in yourself"
+]
+
+def get_inspiration_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " " + json_data[0]['a']
+    return(quote)
 
 @client.event
 async def on_ready():
@@ -14,8 +31,11 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!quote'):
+        quote = get_inspiration_quote()
+        await message.channel.send(quote)
+
+    if any(word in message.content for word in key_words):
+        await message.channel.send(random.choice(start_quotes))
 
 client.run('MTE0NDc3ODYyMzU1NjAwOTk4NA.GPpR3N.DA5JPFoj9KKf1bzUgtV240goBir1T6mHT9QKu4')
-
