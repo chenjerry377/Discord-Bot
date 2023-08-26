090f75848914fbb57b654a4320d900f32d3b994a
chenjerry377 <112854866+chenjerry377@users.noreply.github.com>
2023-08-26 14:44:09 -0400

    Update bot.py
    bot running

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
 import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTE0NDc3ODYyMzU1NjAwOTk4NA.GPpR3N.DA5JPFoj9KKf1bzUgtV240goBir1T6mHT9QKu4')
+
